01b157ddc215de4a3b95fd014ef9ea9b0e133da3
Steve Liu <steveliu397@Steves-MacBook-Air.local>
2023-12-24 22:40:50 -0800

    Modified libraries

--- pybaseball-2.2.6/tests/pybaseball/analysis/projections/marcels/test_age.py
+++
@@ -1,9 +0,0 @@
-from pybaseball.analysis.projections.marcels.age_adjustment import age_adjustment
-
-
-class TestAgeAdjustment:
-    def test_age_adjustment(self):
-        peak_age = 29
-        assert age_adjustment(peak_age) == 1
-        assert age_adjustment(peak_age + 1) < 1
-        assert age_adjustment(peak_age - 1) > 1
--- pybaseball-2.2.6/tests/pybaseball/analysis/projections/marcels/test_marcels_pitching.py
+++
@@ -1,9 +0,0 @@
-from pybaseball.analysis.projections.marcels import MarcelProjectionsPitching
-from pandas import DataFrame
-import pytest
-
-
-def test_pitching_bad_data() -> None:
-    stats_df = DataFrame({"x": [1, 2, 3]})
-    with pytest.raises(ValueError):
-        MarcelProjectionsPitching(stats_df=stats_df)
